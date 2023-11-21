4af84498a7ecd58bfd2815ec94c5dd733d3df00f
Stefano <stefano.dellolioprada@ottogroup.com>
2023-11-21 02:45:50 +0100

    Version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => {
+    test('debe mostrar el componente correctamente con la imagen personalizada', () => {
+        const wrapper = renderer.create(
+            <ProductImage img="https://hola.jpg" />
+        )
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+    test('debe mostrar el componente con la imagen del producto', () => {
+        const wrapper = renderer.create(
+            <ProductCard product={ product2 }>
+                {
+                    () => (
+                        <ProductImage />
+                    )
+                }
+            </ProductCard>
+        )
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+})
